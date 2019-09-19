e15cb3cb59733dd01e03ecfb538b41d4520c1bf0
Daniel Winckler <daniel@danielwinckler.se>
2019-09-19 14:49:17 +0200

    Trying to add Function ImgInfo

--- ImageFunctions/Thumbnail.cs
+++ ImageFunctions/Thumbnail.cs
@@ -25,10 +25,13 @@ using System;
 using System.IO;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Json;
 
 namespace ImageFunctions
 {
-    public static class Thunbnail
+
+    public static class Thumbnail
     {
         private static readonly string BLOB_STORAGE_CONNECTION_STRING = Environment.GetEnvironmentVariable("AzureWebJobsStorage");
 
@@ -103,6 +106,119 @@ namespace ImageFunctions
 
                             image.Mutate(x => x.Resize(thumbnailWidth, height));
                             image.Save(output, encoder);
+                            output.Position = 0;
+                            await blockBlob.UploadFromStreamAsync(output);
+                        }
+                    }
+                    else
+                    {
+                        log.LogInformation($"No encoder support for: {createdEvent.Url}");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                log.LogInformation(ex.Message);
+                throw;
+            }
+        }
+    }
+
+    public static class ImgInfo
+    {
+         private static readonly string BLOB_STORAGE_CONNECTION_STRING = Environment.GetEnvironmentVariable("AzureWebJobsStorage");
+
+        private static string GetBlobNameFromUrl(string bloblUrl)
+        {
+            var uri = new Uri(bloblUrl);
+            var cloudBlob = new CloudBlob(uri);
+            return cloudBlob.Name;
+        }
+
+        private static IImageEncoder GetEncoder(string extension)
+        {
+            IImageEncoder encoder = null;
+
+            extension = extension.Replace(".", "");
+
+            var isSupported = Regex.IsMatch(extension, "gif|png|jpe?g", RegexOptions.IgnoreCase);
+
+            if (isSupported)
+            {
+                switch (extension)
+                {
+                    case "png":
+                        encoder = new PngEncoder();
+                        break;
+                    case "jpg":
+                        encoder = new JpegEncoder();
+                        break;
+                    case "jpeg":
+                        encoder = new JpegEncoder();
+                        break;
+                    case "gif":
+                        encoder = new GifEncoder();
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            return encoder;
+        }
+
+        [FunctionName("ImgInfo")]
+        public static async Task Run(
+            [EventGridTrigger]EventGridEvent eventGridEvent,
+            [Blob("{data.url}", FileAccess.Read)] Stream input,
+            ILogger log)
+        {
+            try
+            {
+                if (input != null)
+                {
+                    var createdEvent = ((JObject)eventGridEvent.Data).ToObject<StorageBlobCreatedEventData>();
+                    var extension = Path.GetExtension(createdEvent.Url);
+                    var encoder = GetEncoder(extension);
+
+                    if (encoder != null)
+                    {
+                        //var thumbnailWidth = Convert.ToInt32(Environment.GetEnvironmentVariable("THUMBNAIL_WIDTH"));
+                        var imginfoContainerName = Environment.GetEnvironmentVariable("IMGINFO_CONTAINER_NAME");
+                        var storageAccount = CloudStorageAccount.Parse(BLOB_STORAGE_CONNECTION_STRING);
+                        var blobClient = storageAccount.CreateCloudBlobClient();
+                        var container = blobClient.GetContainerReference(imginfoContainerName);
+                        var blobName = GetBlobNameFromUrl(createdEvent.Url);
+                        var blockBlob = container.GetBlockBlobReference(blobName);
+
+                        using (var output = new MemoryStream())
+                        using (Image<Rgba32> image = Image.Load(input))
+                        {
+                            /*
+                            var divisor = image.Width / thumbnailWidth;
+                            var height = Convert.ToInt32(Math.Round((decimal)(image.Height / divisor)));
+
+                            image.Mutate(x => x.Resize(thumbnailWidth, height));
+                            image.Save(output, encoder);
+                            output.Position = 0;
+                            */  
+
+                            //Build json for imginfo
+                            Dictionary<string, string> dict = new Dictionary<string, string>();
+                            Type dictType = dict.GetType();
+                            dict.Add("Name", Path.GetFileName(createdEvent.Url));
+                            dict.Add("Width", image.Width.ToString());
+                            dict.Add("Height", image.Height.ToString());
+                            dict.Add("Hash", image.GetHashCode().ToString());
+                            DataContractJsonSerializer json = new DataContractJsonSerializer(
+                                dictType
+                                ,
+                                new DataContractJsonSerializerSettings()
+                                {
+                                    UseSimpleDictionaryFormat = true 
+                                }
+                                );
+                            json.WriteObject(output, dict);
                             output.Position = 0;
                             await blockBlob.UploadFromStreamAsync(output);
                         }
